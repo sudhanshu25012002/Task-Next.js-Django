b55d0619888ce8f0569a46cd0b93711fb57c7a69
Sudhanshu Ranjan <sr25012002@gmail.com>
2024-12-08 17:28:36 +0530

    creating API calls for prodcuts and its services

--- product_management/frontend/services/productService.js
+++ product_management/frontend/services/productService.js
@@ -1,7 +1,38 @@
 // services/productService.js
 
 export async function getProducts() {
-    const res = await fetch('http://localhost:8000/api/products/');
-    const data = await res.json();
-    return data;
+  const res = await fetch("http://localhost:8000/api/products/");
+  const data = await res.json();
+  return data;
+}
+
+export async function addProduct(productData) {
+  const res = await fetch("http://localhost:8000/api/products/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(productData),
+  });
+  const data = await res.json();
+  return data;
+}
+
+export async function editProduct(id, productData) {
+  const res = await fetch(`http://localhost:8000/api/products/${id}/`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(productData),
+  });
+  const data = await res.json();
+  return data;
+}
+
+export async function deleteProduct(id) {
+    const res = await fetch(`http://localhost:8000/api/products/${id}`, {
+        method: 'DELETE',
+    });
+    return res.status === 204; // return if deleted 
 }
