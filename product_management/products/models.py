8cf12cf44850a095d2efc17080062c8b1f5d7763
Sudhanshu Ranjan <sr25012002@gmail.com>
2024-12-08 11:26:08 +0530

    migrations and backend setup done

--- product_management/products/models.py
+++ product_management/products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='product_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def _str_(self):
+        return self.name
